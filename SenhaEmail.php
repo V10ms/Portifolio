74bbf603dd1fb2fd8ee79ebd201d495689d9cce1
Vitor Manoel <103796366+V10ms@users.noreply.github.com>
2024-01-30 17:38:59 -0300

    Delete SenhaEmail.php

--- SenhaEmail.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('USER', 'vitormanoel64544@gmail.com');  // <-- Insira aqui do email que vai responder
-define('PWD', 'jsjy xgpq wgjz adgw');    // <-- Insira aqui a senha do email que vai responder
-?>
